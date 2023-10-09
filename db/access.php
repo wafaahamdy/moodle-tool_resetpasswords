282972b5db1e7e577fb2a89576b79eeb4b92256d
Wafaa Hamdy <eng.wafaa.hamdy@gmail.com>
2023-10-09 09:25:11 +0300

    First phase

---
+++ db/access.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     tool_resetpasswords
+ * @copyright   2023 Wafaa Hamdy <eng.wafaa.hamdy@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+   // Allows the user to upload user pictures.
+  /* $capabilities = array(
+
+  'tool/resetpasswords:bulkresetpassword' => array(
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' =>  'moodle/site:uploadusers',
+    ),
+);
+*/
+
+$capabilities = [
+    'tool/resetpasswords:bulkresetpassword' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'admin' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ],
+    ],
+];
+
